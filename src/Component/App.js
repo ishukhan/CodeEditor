246825f4eb246ba895d621de272a12e9723abfac
ishukhan <ishukk00@gmail.com>
2023-01-16 11:14:22 +0530

    first commit

---
+++ src/Component/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import Editor from "./Editor";
+import useLocalStorage from '../hooks/useLocalStorage'
+
+
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+
+  useEffect(()=>{
+    const timeout= setTimeout(()=>{
+      setSrcDoc(`
+      <html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${js}</script>
+      </html>
+      `)
+
+      return () => clearTimeout(timeout)
+    },250)
+  }, [html, css, js])
+
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          DisplayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor 
+         language="css"
+         DisplayName="CSS"
+         value={css}
+         onChange={setCss}
+        />
+        <Editor
+         language="javascript"
+         DisplayName="JS"
+         value={js}
+         onChange={setJs}
+         />
+      </div>
+
+
+      <div className="pane">
+        <iframe
+        srcDoc={srcDoc}
+        frameBorder="0"
+        title="output"
+        sandbox="allow-scripts"
+        width='100%'
+        height='100%'
+        />
+      </div>
+    </>
+  )
+
+};
+
+export default App;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState} from 'react'
+
+const PREFIX = 'codepen'
+
+export default function useLocalStorage(key, initalValue) {
+
+    const prefixedKey = PREFIX + key
+    
+    const [value, setValue] = useState(()=>{
+        const jsonValue = localStorage.getItem(prefixedKey)
+        if(jsonValue != null) return JSON.parse(jsonValue)
+
+        if(typeof initalValue === 'function'){
+            return initalValue()
+        }
+        else{
+            return initalValue
+        }
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey,value])
+
+
+
+
+  return  [ value, setValue]
+}
